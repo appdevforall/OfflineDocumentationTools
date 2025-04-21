9a43941a4dd67cac3b69c3bbfb6568612a93c7bd
Alex Miller <alexmiller@appdevforall.org>
2025-04-21 12:56:46 -0500

    LLM request client

---
+++ ProcessDocs/ProcessKotlinDocs/KotlinLLMScratch/llm_req.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+import time
+key = open("openaikey.txt", "r").read()
+prompt_file = "linkextractargs_prompt.txt"
+prompt = open(prompt_file, "r").read()
+
+client = OpenAI(
+    api_key=key,  # This is the default and can be omitted
+)
+
+completion = client.chat.completions.create(
+    model="o3-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+message = completion.choices[0].message.content
+open(prompt_file + " " + str(time.time()) + ".txt", "w").write(prompt + "\n\nRESPONSE\n\n" + message)
